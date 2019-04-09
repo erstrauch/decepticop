6ffc88d5e2cbdae298de95922cc508be1fddb194
Alex Mckinley <alex@mckpals.com>
2019-04-08 20:06:09 -0400

    set up user agreement page. Consent form is currently lorem ipsum. TODO for the future: add a camera permission request

--- js/index.js
+++ js/index.js
@@ -4,5 +4,5 @@ function setName()
 {
 	let playerName = document.getElementById("nameBox").value;
 	window.localStorage.setItem("bs-player-name", playerName);
-	console.log(localStorage.getItem("bs-player-name"));
+	window.location.href = "./agreement.html";	
 }
