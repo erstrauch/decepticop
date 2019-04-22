b00733b97902d660ab05ca9999bdd1556376f1cd
Alex Mckinley <alex@mckpals.com>
2019-04-22 10:58:31 -0400

    bots now play cards if they have them. They cannot bluff yet

--- js/bsbot.js
+++ js/bsbot.js
@@ -10,28 +10,44 @@ class bsBot
 	playTurn()
 	{
 		moveAllCards("lastPlayed", "table");
-		let numCards = Math.floor((Math.random() * 4)) + 1;
-		this.playCards(numCards);
+		this.playCards();
 		//alert("Player " + this.playerID + " has played " + numCards + " " + state.getCurrCard() + "'s.");
 
 	}
 
-	playCards(numCards)
+	playCards()
 	{
-		for(var i = 0; i < numCards; i++)
+		const Http = new XMLHttpRequest();
+		const url = "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/pile/" + this.playerID + "/list/";
+		Http.open("GET", url);
+		Http.send();
+		Http.onreadystatechange = (e)=>
 		{
-			const Http = new XMLHttpRequest();
-			const url = "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/pile/" + this.playerID + "/draw/";
-			Http.open("GET", url);
-			Http.send();
-			Http.onreadystatechange = (e)=>
+			if(Http.readyState == 4 && Http.status == 200)
 			{
-				if(Http.readyState == 4 && Http.status == 200)
+				let response = JSON.parse(Http.responseText);
+				var cards = [];
+				if(response.piles[this.playerID] != undefined)
 				{
-					let response = JSON.parse(Http.responseText);
-					var cards = response.cards[0].code + ",";
-					document.getElementById("table").appendChild(document.getElementById(response.cards[0].code));
-					movePiles("lastPlayed", cards, function(){});
+					for(var i = 0; i < response.piles[this.playerID].cards.length; i++)
+					{
+						if(cards[response.piles[this.playerID].cards[i].value] == undefined)
+						{
+							cards[response.piles[this.playerID].cards[i].value] = Array();
+						}
+						cards[response.piles[this.playerID].cards[i].value].push(response.piles[this.playerID].cards[i]);
+					}
+					if(cards[state.getCurrCard()] != undefined)
+					{
+						var playCards = "";
+						let table = document.getElementById("table");
+						for(var i = 0; i < cards[state.getCurrCard()].length; i++)
+						{
+							playCards += cards[state.getCurrCard()][i].code + ",";
+							table.appendChild(document.getElementById(cards[state.getCurrCard()][i].code));
+						}
+						movePiles("table", playCards, function(){});
+					}
 				}
 			}
 		}
