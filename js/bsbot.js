b9f28f0b13e0b282d49f23229f272fb83db7cd1f
Alex Mckinley <alex@mckpals.com>
2019-04-22 20:57:52 -0400

    fixin things

--- js/bsbot.js
+++ js/bsbot.js
@@ -21,6 +21,38 @@ class bsBot
 		this.playCards();
 	}
 
+	checkHand(numPlayed)
+	{
+		const Http = new XMLHttpRequest();
+		const url = "https://deckofcardsapi.com/api/deck/" + deck.deck_id + "/pile/" + this.playerID + "/list/";
+		Http.open("GET", url);
+		Http.send();
+		Http.onreadystatechange = (e)=>
+		{
+			if(Http.readyState == 4 && Http.status == 200)
+			{
+				let response = JSON.parse(Http.responseText);
+				var cards = [];
+				if(response.piles[this.playerID] != undefined)
+				{
+					for(var i = 0; i < response.piles[this.playerID].cards.length; i++)
+					{
+						if(cards[response.piles[this.playerID].cards[i].value] == undefined)
+						{
+							cards[response.piles[this.playerID].cards[i].value] = Array();
+						}
+						cards[response.piles[this.playerID].cards[i].value].push(response.piles[this.playerID].cards[i]);
+					}
+					if(cards[state.getCurrCard()] != undefined && cards[state.getCurrCard()].length + numPlayed > 4)
+					{
+						bs();
+						Alert("Player " + this.playerID + " has called BS!");
+					}	
+				}
+			}
+		}
+	}
+
 	playCards()
 	{
 		const Http = new XMLHttpRequest();
